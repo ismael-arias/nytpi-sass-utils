aeea92c97e374a2f61771a2e4ac99121e3c156d9
Joe Kohlmann <kohlmannj@gmail.com>
2015-10-01 00:52:47 -0400

    Added semver and bumped the version number of the package.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
 var config = require("eyeglass-dev-eslint");
+var bump = require('gulp-bump');
 
 gulp.task("lint", function() {
   return gulp.src(["*.js"])
@@ -12,3 +13,22 @@ gulp.task("lint", function() {
 });
 
 gulp.task("default", ["lint"]);
+
+// semver bump tasks
+gulp.task('bump:patch', function(){
+    gulp.src('./package.json')
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:minor', function(){
+    gulp.src('./package.json')
+        .pipe(bump({type: 'minor'}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:major', function(){
+    gulp.src('./package.json')
+        .pipe(bump({type: 'major'}))
+        .pipe(gulp.dest('./'));
+});
