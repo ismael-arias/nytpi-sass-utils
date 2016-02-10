d3c7058a230daf96cd74706e2101de7479188a97
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2016-02-10 00:42:06 -0500

    Absolutely huge commit.
    - Implementing some deep additional parsing for JSON in eyeglass-exports.js to try to convert strings that represent math expressions (for gutters and outer-margin mostly) to Sass numbers
    - Also converting strings with spaces (for "media" properties) into JavaScript arrays with Sass numbers
    - This is the first version that implements a working size-class mixin, based on the all the parsing code in sass/size-class-v3/_parser.scss
    - Added Size Class v2.x emulation by way of a second config file
    - Added support for specifying your own config file and having it deepmerge into the default config by default

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -3,69 +3,211 @@
 var path = require("path");
 var fs = require("fs");
 var deepmerge = require("deepmerge");
+var Number = require("node-sass").types.Number;
+var math = require("mathjs");
+
+var convertKnownKeysToSassNumbers = function(config) {
+	Object.keys(config).forEach(function(categoryKey) {
+		if (categoryKey === "options") {
+			console.log("Examining options...");
+			Object.keys(config[categoryKey]).forEach(function(optionKey) {
+				var optionValue = config[categoryKey][optionKey];
+				console.log("    Examining options[" + optionKey + "] = " + optionValue);
+				if (typeof optionKey === "string" && typeof optionValue === "string" && (optionKey.match("pixel-grid-ratio") !== null || optionKey === "outer-margin")) {
+					console.log("*** converting " + optionKey + " option...");
+					config[categoryKey][optionKey] = tryToParseSassNumber(tryToMathEvalString(optionValue));
+				}
+			});
+		}
+		// Convert any nested objects' "columns" and "gutters" properties
+		else if (categoryKey === "classes") {
+			var layoutTypes = config.options["layout-types"];
+			var layoutTypeConfigKeys = ["columns", "gutters"];
+
+			Object.keys(config[categoryKey]).forEach(function(classKey) {
+				Object.keys(config[categoryKey][classKey]).forEach(function(classConfigKey) {
+					// Is this a layout-type key?
+					var classConfigValue = config[categoryKey][classKey][classConfigKey];
+					if (layoutTypes.indexOf(classConfigKey) >= 0 && typeof classConfigValue === "object") {
+						console.log("Examining config[" + categoryKey + "][" + classKey + "][" + classConfigKey + "] = " + classConfigValue);
+
+						layoutTypeConfigKeys.forEach(function(layoutTypeConfigKey) {
+							console.log("    Examining " + layoutTypeConfigKey);
+							if (classConfigValue.hasOwnProperty(layoutTypeConfigKey)) {
+								classConfigValue[layoutTypeConfigKey] = tryToParseSassNumber(tryToMathEvalString(classConfigValue[layoutTypeConfigKey]));
+							}
+						});
+
+						// Write back
+						config[categoryKey][classKey][classConfigKey] = classConfigValue;
+					}
+				});
+			});
+		}
+	});
+};
+
+var tryToMathEvalString = function(item) {
+	try {
+		console.log("Trying to math.eval() " + item);
+		var evaluatedItem = math.eval(item);
+		if (typeof evaluatedItem === "number") {
+			return evaluatedItem;
+		} else {
+			return item;
+		}
+	}
+	catch(e) {
+		return item;
+	}
+};
+
+var tryToParseSassNumber = function(item) {
+	// Try to parseFloat
+	var itemAsFloat = parseFloat(item);
+	if (isNaN(itemAsFloat) === false) {
+		// Does this number have units?
+		if (typeof item === "string") {
+			var unit = item.replace(itemAsFloat, "");
+			item = new Number(itemAsFloat, unit);
+		} else {
+			item = new Number(itemAsFloat);
+		}
+
+	}
+
+	return item;
+};
+
+var convertStringToListWithSassNumbers = function(str) {
+	// Removing leading and trailing whitespace.
+	str = str.trim();
+	// Remove leading and trailing parentheses.
+	if (str[0] === "(" && str[str.length - 1] === ")") {
+		str = str.substr(1, str.length - 2);
+	}
+	str = str.split(/[ ,]+/g);
+
+	var finalArray = [];
+
+	str.forEach(function(item) {
+		item = tryToParseSassNumber(item);
+		finalArray.push(item);
+	});
+
+	return finalArray;
+};
+
+var rewriteFamiliesInConfig = function(config) {
+	if (config.hasOwnProperty("families") === false) {
+		return;
+	}
+
+	Object.keys(config.families).forEach(function (familyName) {
+		var familyConfig = config.families[familyName];
+
+		// Is familyConfig a string whose first and last characters aren't matching parentheses?
+		if (typeof familyConfig === "string") {
+			// Split on whitespace into an array and write the value back to the config object.
+			config.families[familyName] = convertStringToListWithSassNumbers(familyConfig);
+		}
+	});
+
+	console.log("Finished rewriting families");
+};
+
+var rewriteMediaPropertiesInConfig = function(config) {
+	if (config.hasOwnProperty("classes") === true) {
+		Object.keys(config.classes).forEach(function(sizeClassName) {
+			config.classes[sizeClassName] = rewriteMediaProperty(config.classes[sizeClassName]);
+		});
+	}
+
+	if (config.hasOwnProperty("breakpoints") === true) {
+		Object.keys(config.breakpoints).forEach(function(breakpointName) {
+			config.breakpoints[breakpointName] = rewriteBreakpoint(config.breakpoints[breakpointName]);
+		})
+	}
+};
+
+var rewriteMediaProperty = function(obj) {
+	if (obj.hasOwnProperty("media") && typeof obj.media === "string") {
+		obj.media = convertStringToListWithSassNumbers(obj.media);
+	}
+
+	return obj;
+};
+
+var rewriteBreakpoint = function(breakpoint) {
+	if (typeof breakpoint === "string") {
+		breakpoint = {
+			"media": breakpoint
+		}
+	}
+
+	return rewriteMediaProperty(breakpoint);
+};
 
 module.exports = function(eyeglass, sass) {
-	var defaultConfigName = "size-class-v3-default-config.json";
-	var defaultConfigPath = path.join(__dirname, defaultConfigName);
+	var defaultConfigName = "nytpi-default.json";
+	var defaultConfigPath = path.join(__dirname, "sass", "size-class-v3", defaultConfigName);
 
 	var sassUtils = require("node-sass-utils")(sass);
 
 	return {
 		sassDir: path.join(__dirname, "sass"),
 		functions: {
-			"hello($name)": function(name, done) {
-				done(sass.types.String('"Why hello, ' + name.getValue() + '"'));
-			},
-			"load-size-classes-config($pathToJsonFile: false, $merge: true)": function(pathToJsonFile, merge, done) {
-				var useArgument = false;
+			"_load-size-class-v3-config($pathToJsonFile: false, $replace: false)": function(pathToJsonFile, merge, done) {
+				var usePathToJsonFileArgument = false;
 				// Did we get a string?
 				if (typeof pathToJsonFile.getValue() === "string") {
 					// Is the value a (quoted) string?
 					var pathValue = pathToJsonFile.getValue().replace(/['\"]?([^'\"]*)['\"]?/, "$1");
 
 					if (pathValue !== "") {
-						// build a relative path to the JSON file
-						pathToJsonFile = path.resolve(path.join(
-							process.cwd(), "scss", pathValue
-						));
+						// build a relative path to the JSON file and see if it exists
+						try {
+							// TODO: Remove assumption that Sass files are in ./scss/ relative to process.cwd()
+							pathToJsonFile = path.resolve(path.join(
+								process.cwd(), "scss", pathValue
+							));
 
-						useArgument = true;
+							// Try to statSync (this will throw an error if the file doesn't exist)
+							fs.statSync(pathToJsonFile);
+						}
+						// If that relative path doesn't work, try a path relative to this module
+						catch(e) {
+							pathToJsonFile = path.resolve(path.join(
+								__dirname, "sass", "size-class-v3", pathValue
+							));
+							// The require() call below will fail if this doesn't exist, so don't worry about that here
+						}
+
+						usePathToJsonFileArgument = true;
 					}
 				}
 
 				// One last check to see if we should use the default config path instead
-				if (useArgument === false) {
+				if (usePathToJsonFileArgument === false) {
 					pathToJsonFile = defaultConfigPath;
 				}
 
 				// Load the JSON configuration file from disk
 				var config = require(pathToJsonFile);
-				// Merge it with the default configuration (if merge.getValue() === true)
-				if (merge.getValue() === true) {
+				// Merge this config with the default configuration (if replace.getValue() === false)
+				if (replace.getValue() === false) {
 					var defaultConfig = require(defaultConfigPath);
 					config = deepmerge(defaultConfig, config);
 				}
 
-				// Postfix for families (add parentheses around strings)
-				if (config.hasOwnProperty("families")) {
-					Object.keys(config.families).forEach(function(familyName) {
-						var familyConfig = config.families[familyName];
+				// Postfix for converting string-encapsulated math expressions to Sass numbers
+				convertKnownKeysToSassNumbers(config);
 
-						// Is familyConfig a string whose first and last characters aren't matching parentheses?
-						if (typeof familyConfig === "string") {
-							// Removing leading and trailing whitespace.
-							familyConfig = familyConfig.trim();
-							// Remove leading and trailing parentheses.
-							if (familyConfig[0] === "(" && familyConfig[familyConfig.length - 1] === ")") {
-								familyConfig = familyConfig.substr(1, familyConfig.length - 2);
-							}
-							// Split on whitespace into an array.
-							familyConfig = familyConfig.split(/[ ,]+/g);
-							// Write the value back to the config object.
-							config.families[familyName] = familyConfig;
-						}
-					});
-				}
+				// Postfix for families (convert JS strings to array of strings; convert anything that should be a Sass number)
+				rewriteFamiliesInConfig(config);
+
+				// Postfix for media queries (convert Sass numbers)
+				rewriteMediaPropertiesInConfig(config);
 
 				// Return to Sass as a map (using castToSass)
 				done(sassUtils.castToSass(config));
